8205bebfcbe2d1c48f655f34590aa932575775f0
LucasdeVittTeles <lucasdevittteles@gmail.com>
2024-09-15 18:57:59 -0300

    Add other entities

--- SalesWeb.Server/Data/Context.cs
+++ SalesWeb.Server/Data/Context.cs
@@ -6,7 +6,8 @@ namespace SalesWeb.Server.Data
     public class Context : DbContext
     {
         public DbSet<Department> Department { get; set; }
-
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
 
         public Context(DbContextOptions<Context> options) : base(options)
         {
--- SalesWeb.Server/Models/Seller.cs
+++ SalesWeb.Server/Models/Seller.cs
@@ -8,15 +8,38 @@
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
 
-        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary)
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
         }
     }
 }
